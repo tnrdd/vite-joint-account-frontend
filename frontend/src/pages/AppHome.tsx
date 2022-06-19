b0e5f1e2a7e07b27ee515e4aff9c024e66492c3d
tnrdd <tnrdd@tutanota.com>
2022-06-19 07:12:40 +0000

    show all navigation tabs even when wallet is not connected

--- frontend/src/pages/AppHome.tsx
+++ frontend/src/pages/AppHome.tsx
@@ -23,20 +23,12 @@ const AppHome = ({
 
 	return (
 		<div className="space-y-4 max-w-3xl mx-auto">
-			{!vcInstance && (
-				<p className="text-center font-bold">Please connect your wallet to use the app</p>
-			)}
-			{vcInstance && !accountId && (
-				<TabNavigation tabNames={['New Account', 'Access Account']} defaultTab={'New Account'} />
-			)}
-			{vcInstance && accountId && (
-				<TabNavigation tabNames={['Deposit', 'Motion']} defaultTab={'Deposit'} />
-			)}
-			{!accountId && vcInstance && activeTab === 'New Account' && <NewAccount />}
-			{!accountId && vcInstance && activeTab === 'Access Account' && <Access />}
+				<TabNavigation tabNames={['New Account', 'Access Account', 'Deposit', 'Motion']} defaultTab={'New Account'} />
+			{activeTab === 'New Account' && <NewAccount />}
+			{activeTab === 'Access Account' && <Access />}
 			{accountId && <div className="flex justify-center font-bold">Account ID: {accountId}</div>}
-			{accountId && activeTab === 'Deposit' && <Deposit />}
-			{accountId && activeTab === 'Motion' && <MotionView />}
+			{activeTab === 'Deposit' && <Deposit />}
+			{activeTab === 'Motion' && <MotionView />}
 		</div>
 	);
 };
