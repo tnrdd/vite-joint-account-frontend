c900c702d0ace8886830e86b3689b05ad6f5aa95
tnrdd <tnrdd@tutanota.com>
2022-06-27 13:19:49 +0000

    update landing page copy

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -11,8 +11,8 @@ const Landing = ({ i18n }: Props) => {
 		<div>
 			<div className="bg-skin-base">
 				<div className="xy flex-col h-[25rem] max-h-screen">
-					<p className="text-4xl font-extrabold">Vite Express</p>
-					<p className="mt-2 text-3xl text-skin-secondary">Let's build something cool</p>
+					<p className="text-4xl font-extrabold">Vite Joint Account</p>
+					<p className="mt-2 text-3xl text-skin-secondary">Protect your funds with a joint account</p>
 					<A
 						to="/app"
 						className="mt-9 font-semibold rounded-lg px-4 py-1 text-xl bg-skin-highlight text-white shadow brightness-button"
